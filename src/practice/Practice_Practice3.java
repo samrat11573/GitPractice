b51ad35b7aef863cb6fd5b8cfe5cd89eee3c825b
Masumreza1 <58913285+Masumreza1@users.noreply.github.com>
2020-05-14 21:36:39 -0400

    My third changes

--- src/practice/Practice_Practice3.java
+++ src/practice/Practice_Practice3.java
@@ -5,6 +5,8 @@ public class Practice_Practice3 {
 	public static void main(String[] args) {
 		
 		System.out.println("Git hub process staging 2");
+		
+		System.out.println("My third changes");
 	}
 
 }
